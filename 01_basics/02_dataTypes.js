ea862149c17ffc4ab990c2fd66f62f36c22d1def
gaurav123tiwari <160328913+gaurav123tiwari@users.noreply.github.com>
2025-04-18 19:30:35 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +
+      3)   // code readability should be high
+
+console.log("Gaurav")
+
+let name="Gaurav"  //string
+let age=18   //int
+let isLoggedIn=false  //boolean
+let state;
+
+// number => 2 to power 53
+// bigint => greater value than number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined yet
+// symbol => unique
+
+console.log(typeof "Gaurav")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
